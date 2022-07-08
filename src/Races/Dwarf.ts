ac1f71cfa29e01f9ade1fb2bc9c5cd0fab1bbff2
Christian Lessa <christianclessa@gmail.com>
2022-07-07 21:42:41 -0300

    Requisito 2 concluido - implementa classes Dwarf, Elf, Halfling e Orc, estendendo da classe Race

---
+++ src/Races/Dwarf.ts
@@ -0,0 +1,23 @@
+import Race from './Race';
+
+class Dwarf extends Race {
+  private _maxLifePoints: number;
+  static countInstances = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+
+    this._maxLifePoints = 80;
+    Dwarf.countInstances += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    return this.countInstances;
+  }
+}
+
+export default Dwarf;
---
+++ src/Races/Elf.ts
@@ -0,0 +1,23 @@
+import Race from './Race';
+
+class Elf extends Race {
+  private _maxLifePoints: number;
+  static countInstances = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+
+    this._maxLifePoints = 99;
+    Elf.countInstances += 1;
+  }
+
+  static createdRacesInstances(): number {
+    return this.countInstances;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+}
+
+export default Elf;
